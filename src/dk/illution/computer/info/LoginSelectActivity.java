841dda651c72d9d766b9f06fb1fcb9b0d6d4b517
fredefl <frederiklassen@gmail.com>
2012-08-28 23:09:37 +0200

    Added search icon to main. Renamed icons.

--- src/dk/illution/computer/info/LoginSelectActivity.java
+++ src/dk/illution/computer/info/LoginSelectActivity.java
@@ -27,7 +27,7 @@ public class LoginSelectActivity extends Activity {
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		getMenuInflater().inflate(R.menu.activity_main, menu);
+		getMenuInflater().inflate(R.menu.activity_login_select, menu);
 		return true;
 	}
 }
