72b7d17526cdabf251a9bcb189838afa3be88346
Frederik <frederik@NiceMintyCandy.(none)>
2012-08-28 22:29:34 +0200

    Removed unused includes

--- src/dk/illution/computer/info/MainActivity.java
+++ src/dk/illution/computer/info/MainActivity.java
@@ -1,17 +1,17 @@
 package dk.illution.computer.info;
 
 import android.app.ActionBar;
-import android.app.FragmentTransaction;
-import android.content.Context;
+//import android.app.FragmentTransaction;
+//import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.NavUtils;
+//import android.support.v4.app.FragmentManager;
+//import android.support.v4.app.NavUtils;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
-import android.view.MenuItem;
+//import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
