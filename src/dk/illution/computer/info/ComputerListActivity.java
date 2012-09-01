0c85c03a77c723afd244943e0fd6f835c7e630a6
fredefl <frederiklassen@gmail.com>
2012-09-01 21:02:03 +0200

    Added a spinner to computer list activity.

--- src/dk/illution/computer/info/ComputerListActivity.java
+++ src/dk/illution/computer/info/ComputerListActivity.java
@@ -1,44 +1,80 @@
 package dk.illution.computer.info;
 
+import android.app.ActionBar;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.NavUtils;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
 
 public class ComputerListActivity extends FragmentActivity
-        implements ComputerListFragment.Callbacks {
+		implements ComputerListFragment.Callbacks, ActionBar.OnNavigationListener {
 
-    private boolean mTwoPane;
+	private boolean mTwoPane;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_computer_list);
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_computer_list);
 
-        if (findViewById(R.id.computer_detail_container) != null) {
-            mTwoPane = true;
-            ((ComputerListFragment) getSupportFragmentManager()
-                    .findFragmentById(R.id.computer_list))
-                    .setActivateOnItemClick(true);
-        }
-    }
+		if (findViewById(R.id.computer_detail_container) != null) {
+			mTwoPane = true;
+			((ComputerListFragment) getSupportFragmentManager()
+					.findFragmentById(R.id.computer_list))
+					.setActivateOnItemClick(true);
+		}
 
-    @Override
-    public void onItemSelected(String id) {
-        if (mTwoPane) {
-            Bundle arguments = new Bundle();
-            arguments.putString(ComputerDetailFragment.ARG_ITEM_ID, id);
-            ComputerDetailFragment fragment = new ComputerDetailFragment();
-            fragment.setArguments(arguments);
-            getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.computer_detail_container, fragment)
-                    .commit();
+		final ActionBar actionBar = getActionBar();
+		actionBar.setDisplayShowTitleEnabled(false);
+		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_LIST);
 
-        } else {
-            Intent detailIntent = new Intent(this, ComputerDetailActivity.class);
-            detailIntent.putExtra(ComputerDetailFragment.ARG_ITEM_ID, id);
-            startActivity(detailIntent);
-        }
-    }
+		// Set up the dropdown list navigation in the action bar.
+		actionBar.setListNavigationCallbacks(
+				// Specify a SpinnerAdapter to populate the dropdown list.
+				new ArrayAdapter<String>(
+						actionBar.getThemedContext(),
+						android.R.layout.simple_list_item_1,
+						android.R.id.text1,
+						new String[]{
+								getString(R.string.computers),
+								getString(R.string.printers),
+								getString(R.string.units),
+						}),
+				this);
+	}
+
+	@Override
+	public void onItemSelected(String id) {
+		if (mTwoPane) {
+			Bundle arguments = new Bundle();
+			arguments.putString(ComputerDetailFragment.ARG_ITEM_ID, id);
+			ComputerDetailFragment fragment = new ComputerDetailFragment();
+			fragment.setArguments(arguments);
+			getSupportFragmentManager().beginTransaction()
+					.replace(R.id.computer_detail_container, fragment)
+					.commit();
+
+		} else {
+			Intent detailIntent = new Intent(this, ComputerDetailActivity.class);
+			detailIntent.putExtra(ComputerDetailFragment.ARG_ITEM_ID, id);
+			startActivity(detailIntent);
+		}
+	}
+
+	public boolean onNavigationItemSelected(int position, long id) {
+		switch (position) {
+			case 0:
+				getActionBar().setLogo(R.drawable.computer);
+				break;
+			case 1:
+				getActionBar().setLogo(R.drawable.printer);
+				break;
+			case 2:
+				getActionBar().setLogo(R.drawable.camera);
+				break;
+
+		}
+		return true;
+	}
 }
