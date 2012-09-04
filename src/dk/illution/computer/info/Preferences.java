a33d993fb2f658e57ccf2655be4b92ce3816fb1e
fredefl <frederiklassen@gmail.com>
2012-09-04 22:25:16 +0200

    Added a up button to the settings view.

--- src/dk/illution/computer/info/Preferences.java
+++ src/dk/illution/computer/info/Preferences.java
@@ -1,8 +1,10 @@
 package dk.illution.computer.info;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
+import android.view.MenuItem;
 
 /**
  * Demonstration of PreferenceFragment, showing a single fragment in an
@@ -13,14 +15,30 @@ public class Preferences extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+ 
 
         // Display the fragment as the main content.
         getFragmentManager().beginTransaction().replace(android.R.id.content,
                 new PrefsFragment()).commit();
+        
+        getActionBar().setHomeButtonEnabled(true);
+        ActionBar actionBar = getActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
     }
+    
+	public boolean onOptionsItemSelected(MenuItem item) {
+    	switch (item.getItemId()) {
+    	   case android.R.id.home:
+    	      finish();
+    	      return true;
+    	}
+    	return false;
+	}
 
 
     public static class PrefsFragment extends PreferenceFragment {
+    	
+
 
         @Override
         public void onCreate(Bundle savedInstanceState) {
