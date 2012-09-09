b8d5f75cf60244a5c490f7f339e86261fb753ec6
fredefl <frederiklassen@gmail.com>
2012-09-09 14:40:43 +0200

    Correctly indented two functions.

--- src/dk/illution/computer/info/LoginActivity.java
+++ src/dk/illution/computer/info/LoginActivity.java
@@ -84,77 +84,49 @@ class SignIn extends AsyncTask<String, Void, HttpResponse> {
 
 
 	public static String _getResponseBody(final HttpEntity entity) throws IOException, ParseException {
-
-	if (entity == null) { throw new IllegalArgumentException("HTTP entity may not be null"); }
-
-	InputStream instream = entity.getContent();
-
-	if (instream == null) { return ""; }
-
-	if (entity.getContentLength() > Integer.MAX_VALUE) { throw new IllegalArgumentException(
-
-	"HTTP entity too large to be buffered in memory"); }
-
-	String charset = getContentCharSet(entity);
-
-	if (charset == null) {
-
-	charset = HTTP.DEFAULT_CONTENT_CHARSET;
-
-	}
-
-	Reader reader = new InputStreamReader(instream, charset);
-
-	StringBuilder buffer = new StringBuilder();
-
-	try {
-
-	char[] tmp = new char[1024];
-
-	int l;
-
-	while ((l = reader.read(tmp)) != -1) {
-
-	buffer.append(tmp, 0, l);
-
-	}
-
-	} finally {
-
-	reader.close();
-
-	}
-
-	return buffer.toString();
-
+		if (entity == null) {
+			throw new IllegalArgumentException("HTTP entity may not be null");
+		}
+		InputStream instream = entity.getContent();
+		if (instream == null) {
+			return "";
+		}
+		if (entity.getContentLength() > Integer.MAX_VALUE) {
+			throw new IllegalArgumentException("HTTP entity too large to be buffered in memory");
+		}
+		String charset = getContentCharSet(entity);
+		if (charset == null) {
+			charset = HTTP.DEFAULT_CONTENT_CHARSET;
+		}
+		Reader reader = new InputStreamReader(instream, charset);
+		StringBuilder buffer = new StringBuilder();
+		try {
+			char[] tmp = new char[1024];
+			int l;
+			while ((l = reader.read(tmp)) != -1) {
+				buffer.append(tmp, 0, l);
+			}
+		} finally {
+			reader.close();
+		}
+		return buffer.toString();
 	}
 
 	public static String getContentCharSet(final HttpEntity entity) throws ParseException {
-
-	if (entity == null) { throw new IllegalArgumentException("HTTP entity may not be null"); }
-
-	String charset = null;
-
-	if (entity.getContentType() != null) {
-
-	HeaderElement values[] = entity.getContentType().getElements();
-
-	if (values.length > 0) {
-
-	NameValuePair param = values[0].getParameterByName("charset");
-
-	if (param != null) {
-
-	charset = param.getValue();
-
-	}
-
-	}
-
-	}
-
-	return charset;
-
+		if (entity == null) {
+			throw new IllegalArgumentException("HTTP entity may not be null");
+		}
+		String charset = null;
+		if (entity.getContentType() != null) {
+			HeaderElement values[] = entity.getContentType().getElements();
+				if (values.length > 0) {
+					NameValuePair param = values[0].getParameterByName("charset");
+					if (param != null) {
+						charset = param.getValue();
+					}
+				}
+		}
+		return charset;
 	}
 
 
