8031808092db89473255d77cdc5cf994de1a35fa
fredefl <frederiklassen@gmail.com>
2012-09-30 23:14:49 +0200

    No fucking way...it compiled!

---
+++ src/dk/illution/computer/info/utils/FontUtils.java
@@ -0,0 +1,93 @@
+/**
+ * Copyright (c) 2011, 2012 Sentaca Communications Ltd.
+ */
+package dk.illution.computer.info.utils;
+
+import android.content.res.AssetManager;
+import android.graphics.Typeface;
+import android.os.Build;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class FontUtils {
+  public static final String TAG_LIGHT = "light";
+
+  public static final String TAG_CONDENSED = "condensed";
+
+  public static final String TAG_BOLD = "bold";
+
+  private static final int ICE_CREAM_SANDWITCH = 14;
+
+  private static Typeface normal;
+
+  private static Typeface bold;
+
+  private static Typeface condensed;
+
+  private static Typeface light;
+
+  private static void processsViewGroup(ViewGroup v, final int len) {
+
+    for (int i = 0; i < len; i++) {
+      final View c = v.getChildAt(i);
+      if (c instanceof TextView) {
+        setCustomFont((TextView) c);
+      } else if (c instanceof ViewGroup) {
+        setCustomFont((ViewGroup) c);
+      }
+    }
+  }
+
+  private static void setCustomFont(TextView c) {
+    Object tag = c.getTag();
+    if (tag instanceof String) {
+      final String tagString = (String) tag;
+      if (tagString.contains(TAG_BOLD)) {
+        c.setTypeface(bold);
+        return;
+      }
+      if (tagString.contains(TAG_CONDENSED)) {
+        c.setTypeface(condensed);
+        return;
+      }
+      if (tagString.contains(TAG_LIGHT)) {
+        c.setTypeface(light);
+        return;
+      }
+    }
+    c.setTypeface(normal);
+  }
+
+  public static void setCustomFont(View topView, AssetManager assetsManager) {
+    if (Build.VERSION.SDK_INT >= ICE_CREAM_SANDWITCH) {
+      return;
+    }
+    initTypefaces(assetsManager);
+
+    if (topView instanceof ViewGroup) {
+      setCustomFont((ViewGroup) topView);
+    } else if (topView instanceof TextView) {
+      setCustomFont((TextView) topView);
+    }
+  }
+
+  private static void initTypefaces(AssetManager assetsManager) {
+    if (normal == null || bold == null || condensed == null || light == null) {
+      normal = Typeface.createFromAsset(assetsManager, "fonts/roboto/Roboto-Regular.ttf");
+      bold = Typeface.createFromAsset(assetsManager, "fonts/roboto/Roboto-Bold.ttf");
+      condensed = Typeface.createFromAsset(assetsManager, "fonts/roboto/Roboto-Condensed.ttf");
+      light = Typeface.createFromAsset(assetsManager, "fonts/roboto/Roboto-Light.ttf");
+    }
+  }
+
+  private static void setCustomFont(ViewGroup v) {
+    final int len = v.getChildCount();
+    processsViewGroup(v, len);
+  }
+
+  public static Typeface getTypefaceNormal(AssetManager assetsManager) {
+    initTypefaces(assetsManager);
+    return normal;
+  }
+}
---
+++ src/dk/illution/computer/info/widget/AccordionView.java
@@ -0,0 +1,186 @@
+/**
+ * Copyright (c) 2011, 2012 Sentaca Communications Ltd.
+ */
+package dk.illution.computer.info.widget;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+import dk.illution.computer.info.utils.FontUtils;
+
+import dk.illution.computer.info.R;
+
+public class AccordionView extends LinearLayout {
+
+  private boolean initialized = false;
+
+  // -- from xml parameter
+  private int headerLayoutId;
+  private int headerFoldButton;
+  private int headerLabel;
+  private int sectionContainer;
+  private int sectionContainerParent;
+  private int sectionBottom;
+
+  private String[] sectionHeaders;
+
+  private View[] children;
+  private View[] wrappedChildren;
+
+  private Map<Integer, View> sectionByChildId = new HashMap<Integer, View>();
+
+  public AccordionView(Context context, AttributeSet attrs) {
+    super(context, attrs);
+
+    if (attrs != null) {
+      TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.accordion);
+      headerLayoutId = a.getResourceId(R.styleable.accordion_header_layout_id, 0);
+      headerFoldButton = a.getResourceId(R.styleable.accordion_header_layout_fold_button_id, 0);
+      headerLabel = a.getResourceId(R.styleable.accordion_header_layout_label_id, 0);
+      sectionContainer = a.getResourceId(R.styleable.accordion_section_container, 0);
+      sectionContainerParent = a.getResourceId(R.styleable.accordion_section_container_parent, 0);
+      sectionBottom = a.getResourceId(R.styleable.accordion_section_bottom, 0);
+      int sectionheadersResourceId = a.getResourceId(R.styleable.accordion_section_headers, 0);
+
+      if (sectionheadersResourceId == 0) {
+        throw new IllegalArgumentException("Please set section_headers as reference to strings array.");
+      }
+      sectionHeaders = getResources().getStringArray(sectionheadersResourceId);
+    }
+
+    if (headerLayoutId == 0 || headerLabel == 0 || sectionContainer == 0 || sectionContainerParent == 0 || sectionBottom == 0) {
+      throw new IllegalArgumentException(
+          "Please set all header_layout_id,  header_layout_label_id, section_container, section_container_parent and section_bottom attributes.");
+    }
+
+    setOrientation(VERTICAL);
+  }
+
+  public View getChildById(int id) {
+    for (int i = 0; i < wrappedChildren.length; i++) {
+      View v = wrappedChildren[i].findViewById(id);
+      if (v != null) {
+        return v;
+      }
+    }
+    return null;
+  }
+
+  public View getSectionByChildId(int id) {
+    return sectionByChildId.get(id);
+  }
+
+  private View getView(final LayoutInflater inflater, int i) {
+    final View container = inflater.inflate(sectionContainer, null);
+    container.setLayoutParams(new ListView.LayoutParams(ViewGroup.LayoutParams.FILL_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT, 0));
+    final ViewGroup newParent = (ViewGroup) container.findViewById(sectionContainerParent);
+    newParent.addView(children[i]);
+    FontUtils.setCustomFont(container, AccordionView.this.getContext().getAssets());
+    if (container.getId() == -1) {
+      container.setId(i);
+    }
+    return container;
+  }
+
+  private View getViewFooter(LayoutInflater inflater) {
+    return inflater.inflate(sectionBottom, null);
+  }
+
+  private View getViewHeader(LayoutInflater inflater, final int position) {
+    final View view = inflater.inflate(headerLayoutId, null);
+    ((TextView) view.findViewById(headerLabel)).setText(sectionHeaders[position]);
+
+    FontUtils.setCustomFont(view, AccordionView.this.getContext().getAssets());
+
+    // -- support for no fold button
+    if (headerFoldButton == 0) {
+      return view;
+    }
+
+    final View foldButton = view.findViewById(headerFoldButton);
+
+    if (foldButton instanceof ToggleImageLabeledButton) {
+      final ToggleImageLabeledButton toggleButton = (ToggleImageLabeledButton) foldButton;
+      toggleButton.setState(wrappedChildren[position].getVisibility() == VISIBLE);
+    }
+
+    final OnClickListener onClickListener = new OnClickListener() {
+
+      public void onClick(View v) {
+        if (wrappedChildren[position].getVisibility() == VISIBLE) {
+          wrappedChildren[position].setVisibility(GONE);
+        } else {
+          wrappedChildren[position].setVisibility(VISIBLE);
+        }
+      }
+    };
+    foldButton.setOnClickListener(onClickListener);
+    view.setOnClickListener(new OnClickListener() {
+
+      public void onClick(View v) {
+
+        onClickListener.onClick(v);
+
+        if (foldButton instanceof ToggleImageLabeledButton) {
+          final ToggleImageLabeledButton toggleButton = (ToggleImageLabeledButton) foldButton;
+          toggleButton.setState(wrappedChildren[position].getVisibility() == VISIBLE);
+        }
+
+      }
+    });
+
+    return view;
+  }
+
+  @Override
+  protected void onFinishInflate() {
+    if (initialized) {
+      super.onFinishInflate();
+      return;
+    }
+
+    final int childCount = getChildCount();
+    children = new View[childCount];
+    wrappedChildren = new View[childCount];
+
+    if (sectionHeaders.length != childCount) {
+      throw new IllegalArgumentException("Section headers string array length must be equal to accordion view child count.");
+    }
+
+    LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+    for (int i = 0; i < childCount; i++) {
+      children[i] = getChildAt(i);
+    }
+    removeAllViews();
+
+    for (int i = 0; i < childCount; i++) {
+      wrappedChildren[i] = getView(inflater, i);
+      View header = getViewHeader(inflater, i);
+      View footer = getViewFooter(inflater);
+      final LinearLayout section = new LinearLayout(getContext());
+      section.setOrientation(LinearLayout.VERTICAL);
+      section.addView(header);
+      section.addView(wrappedChildren[i]);
+      section.addView(footer);
+
+      sectionByChildId.put(children[i].getId(), section);
+
+      addView(section);
+    }
+
+    initialized = true;
+
+    super.onFinishInflate();
+  }
+
+}
