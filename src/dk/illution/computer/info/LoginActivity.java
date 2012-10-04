2d726ed10583e83732ce62394bebee14bf8e71bf
fredefl <frederiklassen@gmail.com>
2012-10-04 20:32:12 +0200

    Made login work with SSL.

--- src/dk/illution/computer/info/LoginActivity.java
+++ src/dk/illution/computer/info/LoginActivity.java
@@ -1,25 +1,13 @@
 package dk.illution.computer.info;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.Scanner;
 
-import org.apache.http.HeaderElement;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicNameValuePair;
-import org.apache.http.protocol.HTTP;
-
-import android.net.ParseException;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.ActionBar;
@@ -34,117 +22,68 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-class SignIn extends AsyncTask<String, Void, HttpResponse> {
+class SignIn extends AsyncTask<String, Void, String> {
 
 	private Context appContext;
-	
+
 	public SignIn (Activity activity) {
 		appContext = activity.getApplicationContext();
 	}
-	
-	protected HttpResponse doInBackground(String... params) {
-		// Create a new HttpClient and Post Header
-		HttpClient httpclient = new DefaultHttpClient();
-		//appContext.getString(R.string.base_url)
-		HttpPost httppost = new HttpPost("https://ci.illution.dk/login/device");
 
-		try {
-			// Add your data
-			List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
-			nameValuePairs.add(new BasicNameValuePair("username", params[0]));
-			nameValuePairs.add(new BasicNameValuePair("password", params[1]));
-			httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
-			httppost.addHeader("Content-Type", "application/x-www-form-urlencoded");
+	protected String doInBackground (String... params) {
+		URL url;
+		HttpURLConnection connection;
 
-			// Execute HTTP Post Request
-			HttpResponse response = httpclient.execute(httppost);
+		try{
+			String response= "";
+
+			// Set the URL
+			url=new URL(appContext.getString(R.string.base_url) + "/login/device");
+
+			// Set parameters
+			String param="username=" + URLEncoder.encode(params[0],"UTF-8")+
+			"&password="+URLEncoder.encode(params[1],"UTF-8");
+
+			// Open connectionection
+			connection=(HttpURLConnection)url.openConnection();
+			connection.setDoOutput(true);
+			connection.setRequestMethod("POST");
+
+			// Set headers
+			connection.setFixedLengthStreamingMode(param.getBytes().length);
+			connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+
+			// Send request
+			PrintWriter out = new PrintWriter(connection.getOutputStream());
+			out.print(param);
+			out.close();
+
+			// Get stream
+			Scanner inStream = new Scanner(connection.getInputStream());
+
+			// Loop through stream and add get the response
+			while(inStream.hasNextLine())
+				response+=(inStream.nextLine());
+
+			// Return the response
 			return response;
-		} catch (ClientProtocolException e) {
-			Log.d("ComputerInfo", "Error while loggin in: ClientProtocolException");
+		}
+		//catch some error
+		catch(MalformedURLException ex){
+			Log.d("ComputerInfo", "Malformed URL error");
 			return null;
-		} catch (IOException e) {
-			Log.d("ComputerInfo", "Error while loggin in: IOException");
-			e.printStackTrace();
-			return null;
-		} catch (Exception e) {
-			Log.d("ComputerInfo", "Error while loggin in");
-			e.printStackTrace();
+		}
+		// and some more
+		catch(IOException ex){
+			Log.d("ComputerInfo", "IO Exception");
 			return null;
 		}
 	}
 
-	protected void onPostExecute(HttpResponse response) {
-		String response_text = "";
-		HttpEntity entity = null;
-		try {
-			entity = response.getEntity();
-			response_text = _getResponseBody(entity);
-		} catch (ParseException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			if (entity != null) {
-				try {
-					entity.consumeContent();
-				} catch (IOException f) {
-					f.printStackTrace();
-				}
-			}
-		} catch (NullPointerException e) {
-			e.printStackTrace();
-		}
+	protected void onPostExecute(String response) {
 		LoginActivity.dialog.hide();
-		Log.d("ComputerInfo", response_text);
+		Log.d("ComputerInfo", response);
 	}
-
-	public static String _getResponseBody(final HttpEntity entity)
-			throws IOException, ParseException {
-		if (entity == null) {
-			throw new IllegalArgumentException("HTTP entity may not be null");
-		}
-		InputStream instream = entity.getContent();
-		if (instream == null) {
-			return "";
-		}
-		if (entity.getContentLength() > Integer.MAX_VALUE) {
-			throw new IllegalArgumentException(
-					"HTTP entity too large to be buffered in memory");
-		}
-		String charset = getContentCharSet(entity);
-		if (charset == null) {
-			charset = HTTP.DEFAULT_CONTENT_CHARSET;
-		}
-		Reader reader = new InputStreamReader(instream, charset);
-		StringBuilder buffer = new StringBuilder();
-		try {
-			char[] tmp = new char[1024];
-			int l;
-			while ((l = reader.read(tmp)) != -1) {
-				buffer.append(tmp, 0, l);
-			}
-		} finally {
-			reader.close();
-		}
-		return buffer.toString();
-	}
-
-	public static String getContentCharSet(final HttpEntity entity)
-			throws ParseException {
-		if (entity == null) {
-			throw new IllegalArgumentException("HTTP entity may not be null");
-		}
-		String charset = null;
-		if (entity.getContentType() != null) {
-			HeaderElement values[] = entity.getContentType().getElements();
-			if (values.length > 0) {
-				NameValuePair param = values[0].getParameterByName("charset");
-				if (param != null) {
-					charset = param.getValue();
-				}
-			}
-		}
-		return charset;
-	}
-
 }
 
 public class LoginActivity extends Activity {
